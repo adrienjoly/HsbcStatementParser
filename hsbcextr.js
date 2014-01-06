211d859f2bbbc1f4612fd1b8b854e3a1231cd7c7
Adrien Joly <adrien@whyd.com>
2014-01-06 23:49:38 +0100

    parsing until i realize that i can't recognize a debit from credit...

--- hsbcextr.js
+++ hsbcextr.js
@@ -2,12 +2,100 @@ var PFParser = require("./node_modules/pdf2json/pdfparser");
 
 // conversion functions
 
+var PAGE_BREAK = "^^^";
+
 function extractTextPages(pdf){
 	return pdf.data.Pages.map(function(page){
 		return page.Texts.map(function(text){
 			return decodeURIComponent(text.R[0].T);
 		});
 	});
+}
+
+function extractText(textPages){
+	return textPages.reduce(function(a,b){
+		return a.concat([PAGE_BREAK]).concat(b);
+	});
+}
+
+var RE_DATE = /\d\d\.\d\d\.\d\d\d\d/g;
+var RE_DATE_SHORT = /\d\d\.\d\d/g;
+
+function parseText(text){
+	var OP_COLS = ["Date", "Détail", "Valeur", "exo", "Débit", "Crédit"];
+	var bankSta = {
+		acctNum: null,
+		dateFrom: null,
+		dateTo: null,
+		balFrom: null,
+		balTo: null,
+		ops: []
+	};
+	var ops = [];
+
+	var i = 0, line = null;
+	function next(){
+		return line = text[i++];
+	}
+
+	function parseUntil(str){
+		do {
+			line = next();
+		} while(line && line.indexOf(str));
+		return line == str;
+	}
+
+	function parseOperation(){
+		var op = {
+			date: null,
+			text: [],
+			dateVal: null,
+			exo: null,
+			debit: null,
+			credit: null
+		};
+		// 1) date
+		op.date = next();
+		// 2) text + dateVal
+		do {
+			next();
+			if (line.match(RE_DATE_SHORT))
+				op.dateVal = line;
+			else
+				op.text.push(line);
+		} while(!op.dateVal);
+		// 3)
+		
+		return op.text.length && op;
+	}
+
+	parseUntil("Votre Relevé de Compte");
+	parseUntil("Compte n°");
+	bankSta.acctNum = next() + next();
+
+	parseUntil("Période");
+	var periode = next().match(RE_DATE);
+	bankSta.dateTo = periode.pop();
+	bankSta.dateFrom = periode.pop();
+
+	parseUntil("SOLDE DE DEBUT DE PERIODE");
+	bankSta.balFrom = next();
+
+	for(;;){
+		var op = parseOperation();
+		if (op)
+			bankSta.ops.push(op);
+		else
+			break;
+	}
+
+	console.log("bank Statement", bankSta);
+	console.log("bank Operations\n", bankSta.ops.map(function(op){
+		return [op.date, op.text.join(" ")];
+	}).join("\n"));
+
+	//textPages.
+	return bankSta;
 }
 
 // init parser
@@ -20,7 +108,9 @@ pdfParser.on("pdfParser_dataError", function(err){
 
 pdfParser.on("pdfParser_dataReady", function(pdf){
 	var textPages = extractTextPages(pdf);
-	console.log("textPages", textPages);
+	var text = extractText(textPages);
+	//console.log("textPages", text);
+	var bankStatement = parseText(text);
 });
 
 // start parsing
