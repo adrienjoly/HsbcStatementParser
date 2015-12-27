0ef71479b8760535f149fc6f07aca8cad2881b29
Adrien Joly <adrien@whyd.com>
2015-12-27 17:58:50 +0100

    added rule to extract the date. but parseTable is not a good option for the following table because number of rows must be provided in accumulator constructor. TO BE CONTINUED

--- parse.js
+++ parse.js
@@ -1,12 +1,12 @@
 var PdfReader = require('pdfreader').PdfReader;
 var Rule = require('pdfreader').Rule;
 
-function displayFollowingLines(lines) {
-	console.log('=>', lines);
+function displayValues(values) {
+	console.log('=>', values);
 }
 
 var processItem = Rule.makeItemProcessor([
-  Rule.on(/^Votre Relevé de Compte$/).accumulateAfterHeading().then(displayFollowingLines),
+	Rule.on(/^Du ([^\s]+) au\s+(.+)/).extractRegexpValues().then(displayValues),
 ]);
 
 new PdfReader().parseFileItems('20151102_RELEVE DE COMPTE_00360070251.pdf', function(err, item){
