8f7aa22d982847b49fdfb9fd3b8c7c80712405e4
Adrien Joly <adrien@whyd.com>
2015-12-27 17:17:38 +0100

    rule-based item processor prints text following "Votre Relevé de Compte"

--- parse.js
+++ parse.js
@@ -1,5 +1,14 @@
 var PdfReader = require('pdfreader').PdfReader;
+var Rule = require('pdfreader').Rule;
+
+function displayFollowingLines(lines) {
+	console.log('=>', lines);
+}
+
+var processItem = Rule.makeItemProcessor([
+  Rule.on(/^Votre Relevé de Compte$/).accumulateAfterHeading().then(displayFollowingLines),
+]);
 
 new PdfReader().parseFileItems('20151102_RELEVE DE COMPTE_00360070251.pdf', function(err, item){
-	console.log(item.text);
+	processItem(item);
 });
